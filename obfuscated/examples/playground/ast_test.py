0f516c4cee5260d5a04afb25ce57317bf9d52ed0
Dana0739 <zetesovadana@gmail.com>
2021-05-23 03:46:22 +0300

    deleting .idea

--- obfuscated/examples/playground/ast_test.py
+++
@@ -1,4 +0,0 @@
-print(Fernet('kV1IuPDgRc9fsYHNG3nifKSuMfm1fcJNeu8TUb7ouBU='.encode('utf-8'))
-      .decrypt('gAAAAABgqZJpPPCDvRH-TA3lsRcSxdAp6UFxry31qzA2cgIZKqNFy1IkV7cTwCpwdoEtRdhKsdh2Rqe8qMlB4EkqP3KLPnMJ3BgUMRFmng9l0cti3L-UmacsHF38DGOVudIEdZ-xi98t1LDXIeiYSV9eCOHO-2rRkWIPwSRHKJy5QRUPiapJYViekQmxIzyHo-QGOGd4CC65lx599DROsBiFmoZxP7sAVQ=='
-               .encode('utf-8'))
-      .decode('utf-8'))
